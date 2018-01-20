35f7f9133ecf23002a9da6595ff669f7d53f07cc
Markus Schwer <markus_schwer@web.de>
2018-01-20 20:49:39 +0100

    Added cellphone support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
+import random
 
-from filereader import random_name, generate_birthdate
+from filereader import random_name, generate_birthdate, rand_phone
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -20,7 +21,12 @@ def result():
     place = random_name("plz-ort.csv").replace(",", " ")
     lastname = random_name("nachnamen.csv")
     birthdate = generate_birthdate(1957, 2003).strftime("%d.%m.%Y")
-    return render_template("result.html", firstname=firstname, lastname=lastname, place=place, picture=picture, birthdate=birthdate)
+
+    cell_prefix = ["151", "160", "170", "171", "175", "152", "151", "162", "172", "173", "174", "157", "163", "177", "178", "159"]
+    cellphone = rand_phone(cell_prefix[random.randint(0, len(cell_prefix) - 1)])
+    landline = None
+
+    return render_template("result.html", firstname=firstname, lastname=lastname, place=place, picture=picture, birthdate=birthdate, cellphone=cellphone, landline=landline)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=False)
