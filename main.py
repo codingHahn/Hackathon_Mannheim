315efe4786b29475107a9e6f669291b881c09173
Nick Hahn <nick.hahn@hotmail.de>
2018-01-20 20:40:16 +0100

    Add random addresses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
+import random
 
-from filereader import random_name, generate_birthdate
+from filereader import random_line, generate_birthdate
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -12,15 +13,16 @@ def index():
 def result():
     gender = request.args.get("gender")
     if gender == "m":
-        firstname = random_name("vornamen_m.csv")
+        firstname = random_line("vornamen_m.csv")
         picture = "/static/male.jpg"
     elif gender == "w":
-        firstname = random_name("vornamen_w.csv")
+        firstname = random_line("vornamen_w.csv")
         picture = "/static/female.jpg"
-    place = random_name("plz-ort.csv").replace(",", " ")
-    lastname = random_name("nachnamen.csv")
+    place = random_line("plz-ort.csv").replace(",", " ")
+    lastname = random_line("nachnamen.csv")
     birthdate = generate_birthdate(1957, 2003).strftime("%d.%m.%Y")
-    return render_template("result.html", firstname=firstname, lastname=lastname, place=place, picture=picture, birthdate=birthdate)
+    address = random_line("strassen.csv") + str(random.randint(1, 47))
+    return render_template("result.html", firstname=firstname, lastname=lastname, address=address, place=place, birthdate=birthdate, picture=picture)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=False)
