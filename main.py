ef270dc0bf86f9a6b6519a51d46d4dd5d22ca5dd
Markus Schwer <markus_schwer@web.de>
2018-01-20 12:58:48 +0100

    Template implementation for flask

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "This is an Identity Generator ;)"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=False)
