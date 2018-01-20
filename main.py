265ba5bfcbd33d0732baa030827f6e5b72eb5f7a
Nick Hahn <nick.hahn@hotmail.de>
2018-01-20 20:27:00 +0100

    Implement pictures

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template
 
-from filereader import random_name
+from filereader import random_name, generate_birthdate
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -13,12 +13,14 @@ def result():
     gender = request.args.get("gender")
     if gender == "m":
         firstname = random_name("vornamen_m.csv")
+        picture = "/static/male.jpg"
     elif gender == "w":
         firstname = random_name("vornamen_w.csv")
+        picture = "/static/female.jpg"
     place = random_name("plz-ort.csv").replace(",", " ")
     lastname = random_name("nachnamen.csv")
     birthdate = generate_birthdate(1957, 2003).strftime("%d.%m.%Y")
-    return render_template("result.html", firstname=firstname, lastname=lastname, place=place, birthdate=birthdate)
+    return render_template("result.html", firstname=firstname, lastname=lastname, place=place, picture=picture, birthdate=birthdate)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=False)
