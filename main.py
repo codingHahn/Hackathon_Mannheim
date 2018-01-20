67f288d404c91598efdb7ed4ce0687fd008d8be5
Markus Schwer <markus_schwer@web.de>
2018-01-20 16:34:49 +0100

    Turned off debug mode

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def result():
     return render_template("result.html", firstname=firstname, lastname=lastname)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=False)
